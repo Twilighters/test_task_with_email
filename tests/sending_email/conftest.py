380f2158fbaaa66bcfaf209170bbf4d357ebf0c7
Twilighters <ilya475@rambler.ru>
2022-02-07 09:15:41 +0300

    allure add

--- tests/sending_email/conftest.py
+++ tests/sending_email/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture()
 def counting_emails(app):
     app.email.input_search_field("Simbirsoft Тестовое задание")  # noqa
     app.email.click_search_button()
